63141c40d6b8bc9a752bb9922e9fd20d4f968689
Szymon Barczak <byisk@byisk.info>
2010-03-02 03:13:52 +0100

    Files used by an include directive added; A lock mechanism added; Not entirely completed, has problem with working well!

--- moveadot.asm
+++ moveadot.asm
@@ -7,8 +7,9 @@
 
 YPosFromBot = $80;
 VisibleMissileLine = $81;
-WidVal = $82;
-WidCnt = $83;
+Lock = $82;
+Lockcounter = $83;
+Figurelock = $84;
 
 Start
 	CLEAN_START
@@ -21,10 +22,11 @@ Start
 	sta YPosFromBot	;Initial Y Position
 	lda #$20
 	sta NUSIZ0	;Quad Width
-	lda #5
-	sta WidCnt
-	lda #1
-	sta WidVal
+	lda #2
+	sta Lock
+	sta Figurelock
+	lda #6
+	sta Lockcounter	
 
 ;VSYNC time
 MainLoop
@@ -38,71 +40,108 @@ MainLoop
 	lda #0
 	sta VSYNC
 
+TestLock
+	lda Lock
+	cmp #1
+	bne TestFigurelock
+	jsr WaitForVblankEnd 
+	dec Lockcounter
+	bne MainLoop
+	lda #6
+	sta Lockcounter
+	inc Lock
+	jmp MainLoop
 
-;Checking vertical position changes
+TestFigurelock
+	lda Figurelock
+	cmp #1
+	bne ChangePosition
+	jmp ChangeColor
 
+ChangePosition
+	ldx #0
+	stx HMM0
+	jsr TestLeft
+	bne NoLeft
+	ldx #$30
+	stx HMM0
+	jmp EnableLock
+NoLeft	jsr TestRight
+	bne NoRight
+	ldx #$D0
+	stx HMM0 
+	jmp EnableLock
+NoRight	jsr TestDown
+	bne NoDown
+	inc YPosFromBot
+	inc YPosFromBot
+	jmp EnableLock
+NoDown	jsr TestUp
+	bne NoUp
+	dec YPosFromBot
+	dec YPosFromBot
+	jmp EnableLock
+NoUp	lda INPT4	
+	beq InptEnabled
+	jmp EnableLock
+	
+InptEnabled
+	dec Figurelock
+	jmp EnableLock
+
+
+ChangeColor
+
+
+TestLeft
+	lda #%01000000
+	bit SWCHA
+	rts
+
+TestRight
+	lda #%10000000
+	bit SWCHA
+	rts	
+
+TestDown
 	lda #%00010000	;Down?
 	bit SWCHA
-	bne SkipMoveDown
-	inc YPosFromBot
-	inc YPosFromBot
-	inc YPosFromBot
-SkipMoveDown
+	rts
 
+TestUp
 	lda #%00100000	;Up?
 	bit SWCHA
-	bne SkipMoveUp
-	dec YPosFromBot
-	dec YPosFromBot
-	dec YPosFromBot
-SkipMoveUp
+	rts
 
-;assum horiz speed will be zero
-	ldx #0
-
-	lda #%01000000	;Left?
-	bit SWCHA
-	bne SkipMoveLeft
-	ldx #$10		;a 1 in the left nibble means go left
-SkipMoveLeft
-
-	lda #%10000000	;Right?
-	bit SWCHA
-	bne SkipMoveRight
-	ldx #$F0		;a -1 in the left nibble means go right...
-SkipMoveRight
-			;(in 4 bits, using "two's complement
-			; notation", binary 1111 = decimal -1
-			; (which we write there as hex "F" --
-			; remember?))
-
-
-	stx HMM0	;set the move for missile 0
-
+EnableLock
+	dec Lock
+	jmp TestLock
 
 ;Other stuff
 
-	lda INPT4		;read button input
-	bmi ButtonNotPressed	;skip if button not pressed
-	lda YPosFromBot		;must be pressed, get YPos
-	sta COLUBK		;load into bgcolor
-	dec WidCnt
-	bne ButtonNotPressed
-changes	lda WidVal
-	cmp #1
-	beq changeto30
-changeto20	lda #1
-	sta WidVal
-	lda #$20
-	sta NUSIZ0
-	jmp reset
-changeto30	lda #2
-	sta WidVal
-	lda #$30
-	sta NUSIZ0
-reset	lda #25
-	sta WidCnt
-ButtonNotPressed
+;	lda INPT4		;read button input
+;	bmi ButtonNotPressed	;skip if button not pressed
+;	lda YPosFromBot		;must be pressed, get YPos
+;	sta COLUBK		;load into bgcolor
+;	lda #2
+;	sta DoLockLoop
+;	dec WidCnt
+;	bne ButtonNotPressed
+;changes	lda WidVal
+;	cmp #1
+;	beq changeto30
+;changeto20	lda #1
+;	sta WidVal
+;	lda #$20
+;	sta NUSIZ0
+;	jmp reset
+;changeto30	lda #2
+;	sta WidVal
+;	lda #$30
+;	sta NUSIZ0
+;reset	lda #25
+;	sta WidCnt
+;ButtonNotPressed
 
 
 
@@ -115,8 +154,6 @@ WaitForVblankEnd
 
 	sta WSYNC
 	sta HMOVE
-
-;Main scanline loop
 
 ScanLoop
 	sta WSYNC
@@ -149,13 +186,12 @@ FinishMissile
 	lda #2
 	sta WSYNC
 	sta VBLANK
-	ldx #30
 
 OverScanWait
 	sta WSYNC
 	dex
 	bne OverScanWait
-	jmp  MainLoop
+	rts
 
 	org $FFFC
 	.word Start
